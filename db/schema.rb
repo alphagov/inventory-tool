cc449b6bc10e8e653728dfc5743d743ab3384455
Stuart Gale <bishboria@gmail.com>
2016-01-25 16:29:32 +0000

    Remove `V1` fields
    - drop version column from Inventories table
    - remove `V1_FIELD_POSITIONS`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113141242) do
+ActiveRecord::Schema.define(version: 20160125161626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20160113141242) do
     t.datetime "date_generated"
     t.boolean  "background_job_in_progress", default: false
     t.string   "flash_notes"
-    t.integer  "version"
   end
 
 end
