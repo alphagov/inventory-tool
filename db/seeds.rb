0b4401c6c49f916044b118dba32731eadb04d735
Stuart Gale <bishboria@gmail.com>
2016-02-01 14:37:47 +0000

    Alter database
    - Drop Activity Logs table as we don't use it
    - Rename title of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ Inventory.delete_all
 
 FactoryGirl.create :skeleton_inventory, name: 'Skeleton spreadsheet', key: GoogleSpreadsheet::SKELETON_KEY
 FactoryGirl.create :inventory, name: 'Dummy spreadsheet', date_generated: 5.days.ago
-FactoryGirl.create :inventory, name: 'Virgin spreadsheet'
+FactoryGirl.create :inventory, name: 'Blank spreadsheet'
