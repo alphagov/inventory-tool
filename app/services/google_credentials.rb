2476a7a35fb29ca1f11c0a7eb5fedc721d93d059
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2016-02-02 13:39:38 +0000

    Avoid writing auth tokens to non-temporary files
    Expect the client id, secret and refresh tokens to be provided as
    environment variables.  Change the auth setup script to use these
    variables, and to spit out the value of the refresh token environment
    variable if needed.

--- app/services/google_credentials.rb
+++ app/services/google_credentials.rb
@@ -1,25 +1,27 @@
 class GoogleCredentials
-  SKELETON_CONFIG_FILE = "#{Rails.root}/config/google_drive_config.json"
-
   def self.saved_session
-    tempfile = generate_tempfile
-    session = GoogleDrive.saved_session(tempfile.path)
-    tempfile.unlink
-    session
+    Tempfile.create('google_secrets') do |tempfile|
+      tempfile.puts credentials.to_json
+      tempfile.flush
+      GoogleDrive.saved_session(tempfile.path)
+    end
   end
 
-  def self.generate_tempfile
-    credentials = JSON.parse(File.read(SKELETON_CONFIG_FILE))
-    credentials['client_id'] = ENV['GOOGLE_DRIVE_CLIENT_ID']
-    credentials['client_secret'] = ENV['GOOGLE_DRIVE_CLIENT_SECRET']
-
-    tempfile = Tempfile.new(%w{ google .json })
-    tempfile.puts credentials.to_json
-    tempfile.close
-    tempfile
+  def self.do_auth_handshake
+    Tempfile.create('google_secrets') do |tempfile|
+      tempfile.puts credentials.to_json
+      tempfile.flush
+      GoogleDrive.saved_session(tempfile.path)
+      data = JSON.parse(File.read(tempfile.path))
+      data['refresh_token']
+    end
   end
 
-  def self.authenticate_new_credentials
-    @session = GoogleDrive.saved_session(SKELETON_CONFIG_FILE)
+  def self.credentials
+    {
+      'client_id' => ENV['GOOGLE_DRIVE_CLIENT_ID'],
+      'client_secret' => ENV['GOOGLE_DRIVE_CLIENT_SECRET'],
+      'refresh_token' => ENV['GOOGLE_DRIVE_REFRESH_TOKEN'],
+    }
   end
 end
