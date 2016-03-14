195a65afa7e8edb94a206a33b663c28441413da9
Tijmen Brommet <tijmen@gmail.com>
2016-03-14 21:34:40 +0000

    Somewhat clear up SpreadsheetUpdater
    This hasn't improved readability much, but perhaps this is a start.

--- app/services/spreadsheet_updater.rb
+++ app/services/spreadsheet_updater.rb
@@ -1,19 +1,28 @@
 class SpreadsheetUpdater
   def initialize(inventory)
     @inventory = inventory
-    @spreadsheet = GoogleSpreadsheet.find_by_key(@inventory.key)
-    @query_rows = @spreadsheet.query_rows
-    @google_iic = @spreadsheet.documents(inventory.id)
-    @govuk_iic = InventoryItemCollection.new_from_search_queries(@inventory, @query_rows)
-    @inventory.log :info, "#{self.class} initialized"
   end
 
   def update!
-    @inventory.log :info, "#{self.class} starting merge"
-    @google_iic.merge_collections!(@govuk_iic)
-    @inventory.log :info, "#{self.class} merge complete - rewriting data back to spreadsheet"
-    @spreadsheet.update(@google_iic)
-    @spreadsheet.calculate_overview_stats
-    @inventory.log :info, "#{self.class} update complete"
+    spreadsheet = GoogleSpreadsheet.find_by_key(@inventory.key)
+
+    google_iic = spreadsheet.documents(@inventory.id)
+
+    govuk_iic = InventoryItemCollection.new_from_search_queries(
+      @inventory,
+      spreadsheet.query_rows
+    )
+
+    @inventory.log :info, "Starting merge"
+
+    google_iic.merge_collections!(govuk_iic)
+
+    @inventory.log :info, "Merge complete - rewriting data back to spreadsheet"
+
+    spreadsheet.update(google_iic)
+
+    spreadsheet.calculate_overview_stats
+
+    @inventory.log :info, "Update complete"
   end
 end
